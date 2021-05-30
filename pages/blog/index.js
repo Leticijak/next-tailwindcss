42922de6373bfc525710caea6c2ae421e95bb142
leticijak <mnogomijeheavy@gmail.com>
2021-05-30 19:37:49 +0200

    slug-get-static-props-replace-md-files

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,27 @@
+import fs from 'fs'
+import path from 'path'
 import Layout from '../../comps/Layout'
 
 export default function BlogPage() {
   return (
-    <div>
+    <Layout>
       <h1>BLOG</h1>
-    </div>
+    </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  // Create slug with replace method
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '')
+    return {
+      slug,
+    }
+  })
+  console.log(posts)
+  return {
+    props: {},
+  }
 }
