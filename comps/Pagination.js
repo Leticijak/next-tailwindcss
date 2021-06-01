9c8280092c597c1fe8255dcafd30bb6ffbd2413b
leticijak <mnogomijeheavy@gmail.com>
2021-06-01 16:44:46 +0200

    pagination-done

--- comps/Pagination.js
+++ comps/Pagination.js
@@ -1,3 +1,39 @@
-export default function Pagination() {
-  return <div>pagination</div>
+import Link from 'next/link'
+
+export default function Pagination({ currentPage, numPages }) {
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = `/blog/page/${currentPage - 1}`
+  const nextPage = `/blog/page/${currentPage + 1}`
+
+  if (numPages === 1) return <> </>
+  return (
+    <div className='mt-6'>
+      <ul className='flex pl-0 list-none my-2'>
+        {!isFirst && (
+          <Link href={prevPage}>
+            <li className="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 cursor-pointer hover:bg-gray-200 '">
+              Previous
+            </li>
+          </Link>
+        )}
+
+        {Array.from({ length: numPages }, (_, i) => (
+          <Link href={`/blog/page/${i + 1}`}>
+            <li className="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 cursor-pointer hover:bg-gray-200 '">
+              {i + 1}
+            </li>
+          </Link>
+        ))}
+
+        {!isLast && (
+          <Link href={nextPage}>
+            <li className="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 cursor-pointer hover:bg-gray-200 '">
+              Next
+            </li>
+          </Link>
+        )}
+      </ul>
+    </div>
+  )
 }
