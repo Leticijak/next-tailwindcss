fab02df23d2e58a8e09873b1dffccf0460ebb96e
leticijak <mnogomijeheavy@gmail.com>
2021-06-01 16:30:13 +0200

    work-on-pagination

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,50 +1,5 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import Layout from '../../comps/Layout'
-import Link from 'next/link'
-import Post from '../../comps/Post'
-import { sortByDate } from '../../utils/index'
+import BlogPage from './page/[page_index]'
+import { getStaticProps } from './page/[page_index]'
 
-export default function BlogPage({ posts }) {
-  console.log(posts)
-  return (
-    <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>BLOG</h1>
-
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
-      </div>
-    </Layout>
-  )
-}
-
-export async function getStaticProps() {
-  const files = fs.readdirSync(path.join('posts'))
-
-  // Create slug with replace method
-  const posts = files.map((filename) => {
-    const slug = filename.replace('.md', '')
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', filename),
-      'utf-8'
-    )
-    // parsing gray matter into an object
-
-    const { data: frontmatter } = matter(markdownWithMeta)
-
-    return {
-      slug,
-      frontmatter,
-    }
-  })
-
-  return {
-    props: {
-      posts: posts.sort(sortByDate),
-    },
-  }
-}
+export { getStaticProps }
+export default BlogPage
---
+++ pages/blog/page/[page_index].js
@@ -0,0 +1,81 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Layout from '../../../comps/Layout'
+import Link from 'next/link'
+import Post from '../../../comps/Post'
+import Pagination from '../../../comps/Pagination'
+import { sortByDate } from '../../../utils/index'
+import { POSTS_PER_PAGE } from '../../../config/index'
+
+export default function BlogPage({ posts, numPages, currentPage }) {
+  console.log(posts)
+  return (
+    <Layout>
+      <h1 className='text-5xl border-b-4 p-5 font-bold'>BLOG</h1>
+
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+      <Pagination currentPage={currentPage} numPages={numPages} />
+    </Layout>
+  )
+}
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
+
+  let paths = []
+
+  for (let i = 1; i <= numPages; i++) {
+    paths.push({
+      params: { page_index: i.toString() },
+    })
+  }
+  console.log(paths)
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const page = parseInt((params && params.page_index) || 1)
+
+  const files = fs.readdirSync(path.join('posts'))
+
+  // Create slug with replace method
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '')
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    )
+    // parsing gray matter into an object
+
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter,
+    }
+  })
+
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
+  const pageIndex = page - 1
+  const orderedPosts = posts
+    .sort(sortByDate)
+    .slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE)
+
+  return {
+    props: {
+      posts: orderedPosts,
+      numPages,
+      currentPage: page,
+    },
+  }
+}
