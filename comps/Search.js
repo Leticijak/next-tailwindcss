4de70b3ad2334975d49d16a390d5414db2534dea
leticijak <mnogomijeheavy@gmail.com>
2021-06-02 10:51:27 +0200

    search

---
+++ comps/Search.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+import { FaSearch } from 'react-icons/fa'
+
+export default function Search() {
+  // useState
+
+  const [searchTerm, setSerachTerm] = useState('')
+  const [searchResults, setSerachResults] = useState([])
+
+  //   useEffect
+
+  useEffect(() => {
+    const getResults = async () => {
+      if (searchTerm === '') {
+        setSerachResults([])
+      } else {
+        const res = await fetch(`/api/search?q=${searchTerm}`)
+        const { results } = await res.json()
+        console.log(results)
+        setSerachResults(results)
+      }
+    }
+    getResults()
+  }, [searchTerm])
+  return (
+    <div className='relative bg-gray-600 p-4'>
+      <div className='container mx-auto flex items-center justify-center md:justify-end'>
+        <div className='relative text-gray-600 w-72'>
+          <form>
+            <input
+              type='search'
+              name='search'
+              id='search'
+              className='bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none w-72'
+              value={searchTerm}
+              onChange={(e) => setSerachTerm(e.target.value)}
+              placeholder='Search Posts....'
+            />
+
+            <FaSearch className='absolute top-0 right-0 text-black mt-3 mr-4' />
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
